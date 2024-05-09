e534ba05f680b64d7cdefb4857349911e630e1d8
Benjamin Haase <benjaminthaase@protonmail.com>
2024-05-09 12:01:30 -0700

    Adds definition for serializing employee

---
+++ include/common.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#define STATUS_SUCCESS 0
+#define STATUS_ERROR -1
+
+typedef struct {
+    char *name;
+    char *address;
+    uint32_t hours;
+} employee_t;
+
+typedef struct {
+    size_t fsize;                /* Size of file in bytes */
+    size_t employee_count       /* count of employees in file */
+} db_header_t;
+
+#endif
---
+++ include/serialize.h
@@ -0,0 +1,6 @@
+#include "common.h"
+#ifndef SERIALIZE_H
+#define SERIALIZE_H
+
+int serialize_employee(employee_t *, char **, size_t *);
+#endif
